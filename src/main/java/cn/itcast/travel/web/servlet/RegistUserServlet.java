f192152ca59052067fa0b5f253d324663c332b52
Kely <564335771@qq.com>
2020-08-11 22:49:24 +0800

    1
    8.11

---
+++ src/main/java/cn/itcast/travel/web/servlet/RegistUserServlet.java
@@ -0,0 +1,41 @@
+package cn.itcast.travel.web.servlet;
+
+import cn.itcast.travel.domain.User;
+import cn.itcast.travel.service.UserService;
+import cn.itcast.travel.service.impl.UserServiceImpl;
+import com.fasterxml.jackson.databind.util.BeanUtil;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+@WebServlet("/registUserServlet")
+public class RegistUserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //获取数据
+        Map<String, String[]> map = request.getParameterMap();
+        //封装对象
+        User user = new User();
+        try {
+            BeanUtils.populate(user,map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        //调用service完成注册
+        UserService service = new UserServiceImpl();
+        boolean flag = service.regist(user);
+        //响应结果
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
